ab2804b0462960db785bbc997bbc3e527701505f
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-08-19 19:25:36 -0300

    Delete index.js

--- apiautenticaçãoremotework/config/index.js
+++
@@ -1,9 +0,0 @@
-const config = {}
-
-config.KEY_FROM_GENERATED_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAxLCJpYXQiOjE2Mjg1NTg3MDIsImV4cCI6MTYyODU1ODc2Mn0.Sp7TqTYVamgcnZncTST3C7spQ0oaKmBk7z_GqtL05Hg";
-
-config.MONGODB_URI = 'mongodb+srv://luisaomonstro:luisaomonstro132727271327lUiS@cluster0.bndh7.mongodb.net/users?retryWrites=true&w=majority'
-
-config.SALT_ROUNDS = 13;
-
-module.exports = config
